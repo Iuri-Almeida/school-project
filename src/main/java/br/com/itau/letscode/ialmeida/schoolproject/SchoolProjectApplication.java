386df92b71c9b23a285bdcccbf6c1d11130bdfbf
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-02 18:02:55 -0300

    feat(feign): add feign client

--- src/main/java/br/com/itau/letscode/ialmeida/schoolproject/SchoolProjectApplication.java
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/SchoolProjectApplication.java
@@ -2,8 +2,10 @@ package br.com.itau.letscode.ialmeida.schoolproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class SchoolProjectApplication {
 
     public static void main(String[] args) {
