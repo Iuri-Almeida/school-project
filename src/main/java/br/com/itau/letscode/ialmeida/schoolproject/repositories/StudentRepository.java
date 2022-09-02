4ccde300acbff509b7b873f78cafb85ffa9a8c27
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-02 15:33:59 -0300

    feat(student): add findByCpf

--- src/main/java/br/com/itau/letscode/ialmeida/schoolproject/repositories/StudentRepository.java
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/repositories/StudentRepository.java
@@ -4,8 +4,12 @@ import br.com.itau.letscode.ialmeida.schoolproject.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+    Optional<Student> findByCpf(Long cpf);
+
 }
