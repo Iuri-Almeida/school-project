8a16da3f93ad0754c0edd071007d4fad7d24e54c
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-02 15:19:45 -0300

    feat(exception): add StudentNotFoundException

---
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/exceptions/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.itau.letscode.ialmeida.schoolproject.exceptions;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String msg) {
+        super(msg);
+    }
+
+}
--- src/main/java/br/com/itau/letscode/ialmeida/schoolproject/exceptions/handler/SchoolExceptionHandler.java
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/exceptions/handler/SchoolExceptionHandler.java
@@ -1,7 +1,24 @@
 package br.com.itau.letscode.ialmeida.schoolproject.exceptions.handler;
 
+import br.com.itau.letscode.ialmeida.schoolproject.exceptions.StandardError;
+import br.com.itau.letscode.ialmeida.schoolproject.exceptions.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @ControllerAdvice
 public class SchoolExceptionHandler {
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(StudentNotFoundException e, HttpServletRequest request) {
+        String error = "Student not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
 }
