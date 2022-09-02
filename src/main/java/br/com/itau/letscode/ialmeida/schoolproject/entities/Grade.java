ffacd159537b916a89be74b9015ae2c8f526d40f
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-02 14:09:44 -0300

    feat(grade): add Grade entity

---
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/entities/Grade.java
@@ -0,0 +1,28 @@
+package br.com.itau.letscode.ialmeida.schoolproject.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_grade")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Grade {
+
+    @Id
+    @GeneratedValue
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
+    private String content;
+    private Double grade;
+
+}
