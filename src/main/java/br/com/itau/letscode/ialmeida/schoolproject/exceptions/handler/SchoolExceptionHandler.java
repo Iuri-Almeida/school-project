faadf6f4ba7cd266f40ef299974d5e07a99d996e
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-02 15:02:58 -0300

    feat(exception): add StandardError and ExceptionHandler

---
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/exceptions/handler/SchoolExceptionHandler.java
@@ -0,0 +1,7 @@
+package br.com.itau.letscode.ialmeida.schoolproject.exceptions.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class SchoolExceptionHandler {
+}
