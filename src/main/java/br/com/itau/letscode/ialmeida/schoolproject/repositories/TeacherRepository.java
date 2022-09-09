4238be646693c52d0a116f57ad084fbe522c06a5
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-09-09 19:09:14 -0300

    feat(teacher): add findByCpf

--- src/main/java/br/com/itau/letscode/ialmeida/schoolproject/repositories/TeacherRepository.java
+++ src/main/java/br/com/itau/letscode/ialmeida/schoolproject/repositories/TeacherRepository.java
@@ -4,8 +4,12 @@ import br.com.itau.letscode.ialmeida.schoolproject.entities.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher, UUID> {
+
+    Optional<Teacher> findByCpf(Long cpf);
+
 }
